3467b1b3b2d9a8fb512f628a07045ad9c4c683c3
Mariko <marikojanes@gmail.com>
2022-05-09 13:51:08 -0600

    First commit
    Upload some of the files to start the project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainContainer from './MainContainer'
+
+function App() {
+  return (
+    <div>
+      <h1>Break an Egg!</h1>
+      <MainContainer />
+    </div>
+  )
+}
+
+export default App
