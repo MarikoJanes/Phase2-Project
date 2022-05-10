6c688bd6fe407d45273905f887ab8d22b6879847
Mariko <marikojanes@gmail.com>
2022-05-10 09:53:16 -0600

    added a link to a single recipe from a search result

--- src/components/ShowSearchedRecipe.js
+++ src/components/ShowSearchedRecipe.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 function ShowSearchedRecipe({searchedRecipe}) {
+    const history = useHistory();
 
     const data = searchedRecipe.map(recipe => {
         return (
-            <li key={recipe.id} className="card" >
-                <img src={recipe.image} alt={recipe.name} style={{height: "236.29px"}} />
+            <li  key={recipe.id} className="card">
+                <img onClick={handleClick} id={recipe.id} src={recipe.image} alt={recipe.name} style={{height: "236.29px"}} />
                 <h3>{recipe.name}</h3>
             </li>
         )
     });
+
+    function handleClick(e) {
+        console.log(e.target.id)
+        history.push(`recipes/${e.target.id}`);
+    }
 
   return (
     <ul className='cards'>
