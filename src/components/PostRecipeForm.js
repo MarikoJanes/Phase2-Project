7af3ca9ec0261f99b0ada1be5dad3ac90a791dd0
Mariko <marikojanes@gmail.com>
2022-05-10 15:30:05 -0600

    added a form structure

--- src/components/PostRecipeForm.js
+++ src/components/PostRecipeForm.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function PostRecipeForm() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+
+    });
+    const [stepInput, setStepInput] = useState([{step1: ""}]);
+    const [stepNum, setStepNum] = useState(1);
+
+    function handleClick() {
+        
+        setStepInput(stepInput => {
+            let tmpObj = {};
+            tmpObj["step"+(stepNum+1+"")] = "";
+            let tmp =  [...stepInput,tmpObj];
+             setStepNum(stepNum => stepNum+=1)
+             return tmp;
+        } );
+    }
+    console.log(stepInput)
+
   return (
-    <div>PostRecipeForm</div>
+   <form >
+        <label>Recipe Name: </label>
+        <input type="text" />
+        <br></br>
+        <label>image URL: </label>
+        <input type="text" />
+        <br></br>
+    {/* alert how to type ingredients? like separate by "," */}
+        <label>Ingredients: </label>
+        <input type="text" />
+        <br></br>
+        <label>Decsriptions: </label>
+        <br></br>
+        {/* <textarea type="text" /> */}
+        
+       
+        {stepInput.map((step, i) => {
+                            return <div key={i}><label>Step {i+1}</label><textarea key={i+1} /> </div>})}
+        <button onClick={handleClick} type="button"> + </button>
+        <button type="submit">Add a recipe</button>
+   </form>
   )
 }
 
